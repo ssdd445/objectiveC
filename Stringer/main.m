36ff06311044b77aff81e4375262962407692bbe
saud <saudwaqarmalik@gmail.com>
2016-09-03 21:52:05 +0500

    Initial Commit

---
+++ Stringer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stringer
+//
+//  Created by Saud Waqar on 9/3/16.
+//  Copyright (c) 2016 Saud Waqar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
